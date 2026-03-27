96d24534fa0425bb79fbcf33eace05cc3c854e9f
jhonerdlamarck <jpdlamarck@gmail.com>
2026-03-27 08:00:44 -0500

    bin: update scripts

---
+++ bin/dotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOTFILES=~/dotfiles
+
+# copy configs
+cp -r ~/.config/nvim $DOTFILES
+cp -r ~/.config/wezterm $DOTFILES
+cp -r ~/.config/tmux $DOTFILES
+cp -r ~/bin $DOTFILES/bin
+
+cd $DOTFILES
+
+# commit per tool if changed
+for dir in nvim wezterm tmux; do
+  if ! git diff --quiet $dir || git ls-files --others --exclude-standard $dir | grep -q .; then
+    git add $dir/
+    git commit -m "$dir: update config"
+  fi
+done
+
+# bin/scripts
+if ! git diff --quiet bin || git ls-files --others --exclude-standard bin | grep -q .; then
+  git add bin/
+  git commit -m "bin: update scripts"
+fi
+
+git push
